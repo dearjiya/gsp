86ac002d220bb956fecf09a5cd5406025de30bf3
Hoonji Yang <bellayhj@gmail.com>
2022-09-13 18:01:26 +0900

    Essential Operators 추가

---
+++ code/cplus_review/Tour/5. Esstential Operators/3. Resource Management.cpp
@@ -0,0 +1,21 @@
+﻿#include <doctest/doctest.h>
+#include <iostream>
+#include <list>
+#include <Header/Vector.h>
+
+
+namespace
+{
+} // anonymous
+
+TEST_CASE("Essential Operations / Resource Management")
+{
+	/**
+	 *
+	 */
+	SUBCASE("what's RAII?")
+	{
+	}
+
+
+}
---
+++ code/cplus_review/Tour/5. Esstential Operators/4. Conventional Operations.cpp
@@ -0,0 +1,23 @@
+﻿#include <doctest/doctest.h>
+#include <iostream>
+#include <list>
+#include <vector>
+
+
+namespace
+{
+} // anonymous
+
+TEST_CASE("Essential Operations / Conventional Operations")
+{
+	/**
+	 * std::swap()을 vector에 대해 해보고 실제 내부 흐름을 따라가 본다
+	 */
+	SUBCASE("std::swap 이해하기")
+	{
+		std::vector<int> v1{ 1, 2, 3 };
+		std::vector<int> v2{ 3, 1, 1 };
+
+		std::swap(v1, v2);
+	}
+}
